d5b018c8d82c07286dc100e79513c819b824515c
Kyle Shilubane <113527628+KyleShilubane@users.noreply.github.com>
2023-06-02 17:58:38 +0200

    Add files via upload

---
+++ Task4/script.js
@@ -0,0 +1,16 @@
+document.getElementById("vtype").value;
+document.getElementById("cov").value;
+
+var vtype = {
+    "type1":"Motorcycle",
+    "type2":"Car",
+    "type3":"Pickup Truck",
+    "type4":"Truck"
+}
+
+var cov = {
+    "condition1":"New",
+    "condition2":"Pre-Owned",
+    "condition3":"Damaged",
+    "condition4":"Scrap Part"
+}
